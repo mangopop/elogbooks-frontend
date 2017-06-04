2648862495251eb5aca4d2797d6d09c31d3fd0d7
mangopop <simon.norton@gmail.com>
2017-06-04 19:28:50 +0100

    added update for status and progress

--- app/modules/job/update/update.controller.js
+++ app/modules/job/update/update.controller.js
@@ -10,10 +10,28 @@
         vm.job = jobResponse
         vm.update = update;
 
+        vm.status = {};
+        vm.progress = {};
+        vm.selectCtrl = {};
+
+        vm.status.options = ["0","1"];
+        vm.status.selected = String(vm.job.status); //model not defaulting if using a number!? Must investigate...
+
+        vm.progress.options = [0,1];
+        vm.progress.selected = String(vm.job.inProgress);
+
+
+
+
         function update() {
+
+            delete vm.job.id;
+            vm.job.status = Number(vm.status.selected);
+            vm.job.inProgress = Number(vm.progress.selected);
+            console.log(vm.job);
+
             $http.put(
-                'http://localhost:8001/job/' + $stateParams.id,
-                {'description':vm.job.description}
+                'http://localhost:8001/job/' + $stateParams.id, vm.job
             ).then(function (response) {
                 $state.go('jobs.view', {id:response.data.id});
             }, function () {
